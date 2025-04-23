9d81ed33b2a4a05eeb3c38cf5f3ce9163bb595fc
Felipe Alexandre <felipeadar108@gmail.com>
2025-04-23 15:04:13 -0300

    feat: implement user module with CRUD operations and validation DTOs

---
+++ src/user/user.controller.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+
+@Controller('users')
+export class UserController {
+  @Get()
+  read() {
+    return {
+      users: [
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Doe' },
+      ],
+    };
+  }
+
+  @Get(':id')
+  readOne(@Param() params) {
+    return { user: {}, params };
+  }
+
+  @Post()
+  create(@Body() { name, email, password }: CreateUserDTO) {
+    return { user: {}, name, email, password };
+  }
+
+  @Put(':id')
+  update(@Param() params, @Body() { name, email, password }: UpdatePutUserDTO) {
+    return {
+      method: 'PUT',
+      params,
+      name,
+      email,
+      password,
+    };
+  }
+
+  @Patch(':id')
+  updatePartial(
+    @Param() params,
+    @Body() { name, email, password }: UpdatePatchUserDTO,
+  ) {
+    return { user: {}, params, name, email, password };
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    return { user: {}, params };
+  }
+}
