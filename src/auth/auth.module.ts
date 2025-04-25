f35ea021a73c64e1af9a27bfcf54d4717776c73b
Felipe Alexandre <felipeadar108@gmail.com>
2025-04-25 11:20:01 -0300

    feat: implement authentication endpoints with login, register, forget, and reset functionalities

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt/dist';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
     UserModule,
+    PrismaModule,
     JwtModule.register({
       secret: 'your-secret-key',
       signOptions: { expiresIn: '1h' },
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
+import { User } from 'generated/prisma';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthRegisterDTO } from './dto/auth-register.dto';
 import { UserService } from 'src/user/user.service';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService,
   ) {}
 
-  async createToken() {}
+  createToken(user: User): { accessToken: string } {
+    const payload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const options: JwtSignOptions = {
+      expiresIn: '7 days',
+      subject: String(user.id),
+      issuer: 'login',
+      audience: 'users',
+    };
+
+    const token = this.jwtService.sign(payload, options);
+    return { accessToken: token };
+  }
+  async checkToken() {}
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+        password,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('E-mail ou senha incorretos');
+    }
+
+    return this.createToken(user);
+  }
+
+  async register(data: AuthRegisterDTO) {
+    const user = await this.userService.create(data);
+
+    return this.createToken(user);
+  }
+
+  async forget(email: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('E-mail está incorreto.');
+    }
+
+    //TO DO: Enviar e-mail de recuperação
+
+    return true;
+  }
+
+  async reset(password: string, token: string) {
+    //TO DO: Validar o token
+
+    const id = '0';
+    const user = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        password,
+      },
+    });
+    return this.createToken(user);
+  }
 }
