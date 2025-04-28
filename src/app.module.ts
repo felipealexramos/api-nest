92da3b93ce5a759a4a6dbcc9f047bb9aa59cd36d
Felipe Alexandre <felipeadar108@gmail.com>
2025-04-28 16:03:40 -0300

    feat: add throttling support with @nestjs/throttler for rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,31 @@ import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)], // Evita circular dependency
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60, // 1 minute
+          limit: 100, // 10 requests per minute
+        },
+      ],
+    }),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+  ], // Evita circular dependency
   controllers: [AppController],
-  providers: [AppService, UserService, PrismaService],
+  providers: [
+    AppService,
+    UserService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['*'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(process.env.PORT ?? 3000);
